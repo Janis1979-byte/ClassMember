4a7a1024681923752dcfb14a653f06dbe9636571
Janis1979-byte <janis.ruselis@inbox.lv>
2023-03-21 21:35:05 +0200

    Izmaina

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Teksts 1\n");
         System.out.print("\\ kas notiek\\");
-
+System.out.println("Kae fuss");
     }
 }
