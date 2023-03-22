481386cfc5f65a426e3dc2e12bfe5025447be31b
Janis1979-byte <janis.ruselis@inbox.lv>
2023-03-22 15:26:32 +0200

    Atjaunots

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,27 @@ public class Main {
         System.out.print("Teksts 1\n");
         System.out.print("\\ kas notiek\\");
 System.out.println("Kae fuss");
+/*int x = 3;
+int y =4;
+x =5;*/
+
+int x, y;
+ x=2;
+ y=4;
+
+
+        boolean falg = true;
+char symbol = 'b';
+System.out.println((int)symbol);
+        System.out.println(falg);
+
+String text = "Hello World";
+System.out.println("x= " + x);
+
+System.out.println("cau ka iet " + text + " !" +symbol );
+ float b = 3.221111f;
+System.out.println(Integer.MAX_VALUE);
+
+
     }
 }
