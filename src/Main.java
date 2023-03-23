0b23a3664e8c3a03649bbaee8214f938e8715377
Janis1979-byte <janis.ruselis@inbox.lv>
2023-03-23 11:09:49 +0200

    1 majasdarbs

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Teksts 1\n");
+     /*   System.out.print("Teksts 1\n");
+
         System.out.print("\\ kas notiek\\");
 System.out.println("Kae fuss");
-/*int x = 3;
+*//*int x = 3;
 int y =4;
-x =5;*/
+x =5;*//*
 
 int x, y;
  x=2;
@@ -23,7 +26,33 @@ System.out.println("x= " + x);
 
 System.out.println("cau ka iet " + text + " !" +symbol );
  float b = 3.221111f;
-System.out.println(Integer.MAX_VALUE);
+System.out.println(Integer.MAX_VALUE);*/
+/* System.out.print("Ievadi savu vardu");
+ Scanner scanner = new Scanner(System.in);
+String vards= scanner.nextLine();
+System.out.println("ievadi savu uzvardu");
+String uzvard = scanner.nextLine();
+System.out.println("Vecums vienads");
+int vecums = scanner.nextInt();
+scanner.close();
+System.out.println(vards.toUpperCase() + " " + uzvard.toUpperCase() + ": " + vecums);*/
+
+        /*System.out.print("Ievadiet skaitli x  ");
+        Scanner scasner = new Scanner(System.in);
+        int x = scasner.nextInt();
+        System.out.print("ievadiet skaitli Y ");
+        int y = scasner.nextInt();
+        scasner.close();
+       System.out.println("ciparu summa ir:" + (x+y));*/
+System.out.println(5%3);
+
+int x= 1;
+
+System.out.println(x++);
+System.out.println(++x);
+System.out.println(x);
+System.out.println(x++);
+System.out.println(x);
 
 
     }
---
+++ src/MajasDarbs1.java
@@ -0,0 +1,17 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class MajasDarbs1 {
+    public static void main(String[] args) {
+
+      char degreeFarnheit = 8457;
+      char degreecelsius = 8451;
+       System.out.print("Ievadiet grādus farenheita" +  degreeFarnheit+ ": ");
+        DecimalFormat f = new DecimalFormat("##.00");
+        Scanner scanner = new Scanner(System.in);
+        float x = scanner.nextFloat();
+        scanner.close();
+        float y = (5*(x-32))/9;
+        System.out.println("Celsija gradi" + degreecelsius + ":"  + f.format(y));
+    }
+}
