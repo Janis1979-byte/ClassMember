39fedc129758641baf6fc57d2aa7468f0ac6412a
Janis1979-byte <janis.ruselis@inbox.lv>
2023-04-08 14:01:12 +0300

    9nodarbiba

--- src/nodarbiba9.java
+++ src/nodarbiba9.java
@@ -1,5 +1,206 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class nodarbiba9 {
     public static void main(String[] args) {
-       
+/*int i = 0;
+while(i<=10){
+    System.out.println(i);
+    i++;*/
+      /*  Scanner scanner = new Scanner(System.in);
+        System.out.println("Ievadi tekstu :");
+        String text = scanner.nextLine();
+        while(!text.equalsIgnoreCase("exit")) {
+            System.out.println("ievadi velreiz");
+            text = scanner.nextLine();
+            // if(text.equalsIgnoreCase("exit")) {
+
+        }
+        scanner.close();
+        System.out.println("ievade pabeigta");*/
+
+   /*     Scanner scanner = new Scanner(System.in);
+        String text = "";
+        //System.out.println("Ievadi");
+        do {
+
+            text = scanner.nextLine();
+            //System.out.println("Try again!");
+
+        } while (!text.equalsIgnoreCase("exit"));
+        scanner.close();
+
     }
+
+        }*/
+
+//        int i = 0;
+////System.out.println("Ievadi");
+//        do {
+//            System.out.println(i++);
+//
+//        } while (i<5);
+//    }}
+// 1 uzdevums
+    /*    int i = 1;
+        int x = 0;
+        do {
+            System.out.println("Skaitlis :" + i);
+            x += i;
+            i++;
+        } while (i <= 10);
+
+        System.out.println("skaitlu summ " + (x));
+    }
+
+}*/
+
+// 2 uzdevums
+/* Scanner scaner = new Scanner(System.in);
+ System.out.println("Ludzu ievadi tekstu :");
+ String teksts = scaner.nextLine();
+ scaner.close();
+int i = 0;
+ do {
+     System.out.println(Character.toUpperCase(teksts.charAt(i)));
+     i++;
+ } while (i < teksts.length());
+        }}*/
+
+//3 uzdevums
+    /*    Scanner scanner = new Scanner(System.in);
+        System.out.println("Ievadi skaitlis");
+        int x = scanner.nextInt();
+        int y = 1;
+        scanner.close();
+        do {
+            System.out.printf("%d * %d = %d\n", x, y, x * y);
+            y++;
+        } while (x >= y);*/
+
+
+
+      /* Random rnd = new Random();
+
+
+int y = rnd.nextInt(11) -5;*/
+
+       /* int x = new Random().nextInt(11)-5;
+        System.out.println(x);*/
+      /*  int x=  Math.abs(5-10);
+        //int x =5;
+        int y = 7;
+
+       // System.out.println(x);
+       // System.out.println(Math.max(x,y));
+        System.out.printf("x: %d ; y: %d\n", y , x);
+        System.out.println(Math.floor(5.99));
+        System.out.println(Math.round(5.99));
+        System.out.printf("%1.2f\n", Math.pow(3, 2.8));
+        System.out.println(Math.sqrt(9));*/
+/*        Random rnd = new Random();
+        int x = rnd.nextInt(10);
+        System.out.println("Ievadi skaitli:");
+        Scanner scanner = new Scanner(System.in);
+int y;*/
+
+/*        while (y != x) {
+            if(y >9 || y<0)
+            {
+                System.out.println("Ievadi pareizu ciparu");
+            } else {
+
+                System.out.println("cipars nav uzminets!: ");
+            }
+            y= scanner.nextInt();
+        }*/
+
+       /* do {
+            y = scanner.nextInt();
+            if (y > 9 || y < 0) {
+                System.out.println("Ievadi pareizu ciparu");
+            } else if(y != x){
+                System.out.println("cipars nav uzminets!: ");
+
+            }
+        } while (y != x);
+
+                System.out.println("cipars uizminets: " + x + "\n Ievaditais cipars: " + y);
+        scanner.close();*/
+       /* while(true){
+         y = scanner.nextInt();
+          if(y == x) {
+              break;
+          }else{
+              System.out.println("cipars nav uzminets!: ");
+          }
+
+        }
+        scanner.close();
+        System.out.println("cipars uizminets: " + x + "\n Ievaditais cipars: " +y);*/
+       String[]masins = {"volvo", "bmw" };
+
+
+        String car1 = "Volvo";
+        String car2 = "audi";
+        String car3 ="Toyota";
+        String car4 = "opel";
+
+
+
+
+    /*    String[] masivs = new String[3];
+        masivs[0] = "Volvo";
+        masivs[1] = "opel";
+        masivs[2] = "toyo";
+      *//*  System.out.println(Arrays.toString(masivs));
+        System.out.println(masivs[2]);
+        System.out.println(Arrays.toString(masins));
+        System.out.println(masins[1]);*//*
+
+    *//*    int[] atzimes = {1,2,3,5,6,7,8};
+        for(int i = 0; i <atzimes.length; i++) {
+            System.out.println(atzimes[i]);*//*
+
+            for(String element: masivs) {
+             System.out.println(element);*/
+int x= 0;
+int[] grades = {1,8,80,17,10,6,7,8} ;
+int a = grades.length;
+for(int i =0; i<grades.length; i++) {
+ System.out.println(grades[i]);
+ x += grades[i];
 }
+float average = (float)x/a;
+
+ System.out.println(x/a);
+
+     System.out.printf("%.3f", average);
+// System.out.println( grades.length/sum);
+}
+
+ /*    int TotalSum=0;
+     int A[]={1,2,3,4,5};
+     int n= A.length;
+     for (int i=0; i<n;i++){
+      TotalSum=TotalSum+A[i];
+     }
+     System.out.println("The average="+ (float)(TotalSum/n));
+    }
+}*/
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
