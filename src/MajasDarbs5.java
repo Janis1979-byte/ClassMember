225fac0eebb196b1ca3bc023432c8a0853596bb1
Janis1979-byte <janis.ruselis@inbox.lv>
2023-04-16 20:06:11 +0300

    11nodarbiba2

--- src/MajasDarbs5.java
+++ src/MajasDarbs5.java
@@ -6,16 +6,18 @@ public class MajasDarbs5 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Random rnd = new Random();
-        int x = rnd.nextInt(4) + 3;
+        int x = rnd.nextInt(3)+5 ;
         int[][] masivs = new int[x][x];
         //System.out.println(Arrays.deepToString(masivs));
         System.out.println("A. Aizpildit masiva elementa vertibas manuali?");
         System.out.println("B. Aizpildit masiva elementa vertibas nejausi?");
         String izveleMat = scan.nextLine();
-        while (!izveleMat.equalsIgnoreCase("A")) {
-            System.out.println("Ludzu noradiet atbilstosu vertibu");
-           izveleMat = scan.nextLine();
+        while(!(izveleMat.equalsIgnoreCase("A") || izveleMat.equalsIgnoreCase("B"))){
+            System.out.printf("Ievaditais burts %s nav atbilstoss, ievadiet [A,B]: ", izveleMat );
+            izveleMat = scan.nextLine();
         }
+        if (izveleMat.equalsIgnoreCase("A")) {
+            System.out.printf("Ievaditais burts %s atbisltos aizpildam automatiski [A] varinatu \n" , izveleMat);
             for (int i = 0; i < masivs.length; i++) {
                 for (int j = 0; j < masivs[i].length; j++) {
                     masivs[i][j] = rnd.nextInt(101) + 1;
@@ -23,30 +25,42 @@ public class MajasDarbs5 {
                 }
                 System.out.println();
             }
-         while (!izveleMat.equalsIgnoreCase("B")) {
-        System.out.println("Ludzu noradiet atbilstosu vertibu");
-        izveleMat = scan.nextLine();
-
-        /*
-
-
-
-        char a = 'A';
-        char b = 'B';
-        Character.toString(a);
-
-        System.out.println(teksts1);
-        System.out.println(teksts2);
-        String izvele1 = scan.nextLine();
-        if(izvele1 == a ) {
-            for(int i =0; i< masivs.length; i++){
-                for(int j =0; j<masivs[i].length; j++){
-               masivs[i][j]= rnd.nextInt();
+        }
+        if (izveleMat.equalsIgnoreCase("B")) {
+            System.out.printf("Ievaditais burts %s atbisltos aizpildam [B] varinatu \n", izveleMat);
+            for (int i = 0; i < masivs.length; i++) {
+                for (int j = 0; j < masivs[i].length; j++) {
+                    System.out.printf("array[%d][%d] =", i, j);
+                    masivs[i][j] = scan.nextInt();
                 }
+            }
+            scan.close();
+            for (int[] row : masivs) {
+                for (int column : row) {
+                    System.out.printf("%5d", column);
+                }
+                System.out.println();
 
             }
-        }
-        System.out.printf("%5d", masivs);*/
 
-    } } }
+        }
+            ;
+           /* for (int i = 0; i < masivs.length; i++) {
+                for (int j = 0; j < masivs[i].length; j++) {
+                    System.out.printf("%5d", masivs[i][j]);*/
+
+        /*for(int i =0; i< masivs.length; i++) {
+            for(in)*/
+
+        }
+
+
+        }
+                //System.out.println();
+
+
+
+
+
+
 
---
+++ src/Nodarbiba12.java
@@ -0,0 +1,79 @@
+import java.util.Random;
+import java.util.Scanner;
+
+//public class Nodarbiba12 {
+  //  public static void main(String[] args) {
+    /*    Scanner scaner = new Scanner(System.in);
+        Random rnd = new Random();
+       // int[][] arry2D = new int[2][2];
+int[][] arry2D = {{1,3,4} , {1,4,4}};
+        *//*for(int i=0; i<arry2D.length; i++) {
+            for (int j = 0; j < arry2D[i].length; j++) {
+                arry2D[i][j] = scaner.nextInt();
+*//*
+
+//scaner.close();
+            for (int[] row : arry2D) {
+                for (int column : row) {
+                    System.out.printf("%5d", column);
+                }
+                System.out.println();
+*/
+     /*   int[][] array2D = {
+                {1, 2, 3},
+                {6, 5, 4}
+        };
+        *//*for (int i = 0; i < array2D.length; i++) {
+            for (int j = 0; i < array2D[i].length; i++) {*//*
+        int[] temp = array2D[0];
+        array2D[0] = array2D[1];
+        array2D[1] = temp;
+
+        for (int[] row : array2D) {
+            for (int column : row) {
+                System.out.printf("%5d", column);
+            }
+            System.out.println();*/
+      /*  Random rnd = new Random();
+        int n = rnd.nextInt(5) + 2;
+        int m = rnd.nextInt(5) + 2;
+        int[][] arrayD = new int[n][m];
+
+        for (int i = 0; i < arrayD.length; i++) {
+            for (int j = 0; j < arrayD[i].length; j++) {
+                arrayD[i][j] = rnd.nextInt(101);
+            }
+        }
+        for (int[] row : arrayD) {
+            for (int column : row) {
+                System.out.printf("%5d", column);
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < arrayD.length/2; i++) {
+                int[] temp = arrayD[i];
+                arrayD[i] = arrayD[arrayD.length-i-1];
+            arrayD[arrayD.length-i-1]  = temp;
+
+        }
+        System.out.println();
+        for (int[] row : arrayD) {
+            for (int column : row) {
+                System.out.printf("%5d", column);
+            }
+            System.out.println();
+*/
+
+      /*  String data = "101";
+       // int flag = Integer.parseInt(data);
+      //  if(Integer.parseInt(data) >100) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+        }
+    }*/
+
+   // }
+//}
